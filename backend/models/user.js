218a1f3601332611ca7b183081d1a9bd97faee0b
Kortney Canyon <kcanyon_2000@yahoo.com>
2024-07-01 02:10:01 -0500

    Created User models

---
+++ backend/models/user.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+}, {
+    tableName: 'Users',
+    timestamps: true,
+});
+
+module.exports = User;
