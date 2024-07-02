0f82d283becfd74022aafc6b7bea982187077eaf
Kortney Canyon <kcanyon_2000@yahoo.com>
2024-07-01 20:32:54 -0500

    Created routes and controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,45 @@
+const userService = require('../services/userService');
+
+const createUser = async (req, res) => {
+    try {
+        const user = await userService.createUser(req.body);
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+};
+
+const getUserById = async (req, res) => {
+    try {
+        const user = await userService.getUserById(req.params.id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+};
+
+const updateUser = async (req, res) => {
+    try {
+        const user = await userService.updateUser(req.params.id, req.body);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const deleteUser = async (req, res) => {
+    try {
+        const user = userService.deleteUser(req.params.id);
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = {
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser,
+}
+
